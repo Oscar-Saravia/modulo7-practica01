ad3fdcbebb29f3eb537e969e142f4866019ed46d
Oscar Saravia <osaraviav13@gmail.com>
2024-08-20 23:49:39 -0400

    Modulo 7: Primera practica React-ThinkingInReactV2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Products from './screens/Products';
 
-function App() {  
+const App = () => {  
   return (
     <div className="App">
       <header className="App-header">
